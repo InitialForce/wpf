22925c9d0e41fdc28967681b14abdcc9d0bc08ed
h3xds1nz <gamingshard@protonmail.com>
2024-10-22 20:16:24 +0200

    Adjust style to be on par with other change
    (cherry picked from commit 5b0b50e0cee0378e1ec987480574d6aa262666ac)

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/documents/DocumentViewerHelper.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/documents/DocumentViewerHelper.cs
@@ -271,13 +271,9 @@ namespace MS.Internal.Documents
         /// <param name="findToolBar">FindToolBar instance.</param>
         internal static void ShowFindUnsuccessfulMessage(FindToolBar findToolBar)
         {
-            string messageString;
-
             // No, we did not find anything. Alert the user.
-            messageString = findToolBar.SearchUp ?
-                        SR.DocumentViewerSearchUpCompleteLabel :
-                        SR.DocumentViewerSearchDownCompleteLabel;
-            messageString = String.Format(System.Globalization.CultureInfo.CurrentCulture, messageString, findToolBar.SearchText);
+            string messageString = findToolBar.SearchUp ? SR.DocumentViewerSearchUpCompleteLabel : SR.DocumentViewerSearchDownCompleteLabel;
+            messageString = string.Format(CultureInfo.CurrentCulture, messageString, findToolBar.SearchText);
 
             HwndSource hwndSource = PresentationSource.CriticalFromVisual(findToolBar) as HwndSource;
             IntPtr hwnd = (hwndSource != null) ? hwndSource.Handle : IntPtr.Zero;
