316838a3933404035ac73fddbf265d9044b93639
h3xds1nz <gamingshard@protonmail.com>
2025-04-07 18:35:27 +0200

    [StyleCleanUp] Add readonly modifiers in PresentationBuildTasks
    (cherry picked from commit ed6f3680c2a26877c94544a51f6c5fda3ac5b674)

--- src/Microsoft.DotNet.Wpf/src/PresentationBuildTasks/MS/Internal/MarkupCompiler/MarkupCompiler.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationBuildTasks/MS/Internal/MarkupCompiler/MarkupCompiler.cs
@@ -3414,9 +3414,11 @@ namespace MS.Internal
             }
 
             private bool _isAllowedNameScope = true;
-            private Type _elementType = null;
-            private string [] _typeArgsList = null;
-            private string _localElementFullName = string.Empty;
+
+            private readonly Type _elementType;
+            private readonly string[] _typeArgsList;
+            private readonly string _localElementFullName = string.Empty;
+
             protected CodeTypeReference _ctrElemTypeRef = null;
         }
 
@@ -3490,8 +3492,9 @@ namespace MS.Internal
                 set { _subClass = value; }
             }
 
-            private CodeTypeDeclaration _codeClass;
-            private CodeNamespace _codeNS;
+            private readonly CodeTypeDeclaration _codeClass;
+            private readonly CodeNamespace _codeNS;
+
             private CodeMemberMethod _initializeComponentFn = null;
             private CodeMemberMethod _hookupFn = null;
             private CodeMemberMethod _styleConnectorFn = null;
