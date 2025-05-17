ad0d16f95273a463d0d12fbe9192a1f8ffd25ea5
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:25:17 +0200

    write-register-dps (run)
    (cherry picked from commit 08e38b756c390d954a97f83c4d8cc352479cd379)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Quaternion.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Quaternion.cs
@@ -326,7 +326,6 @@ namespace System.Windows.Media.Media3D
         #region Dependency Properties
 
 
-
         #endregion Dependency Properties
 
         //------------------------------------------------------
