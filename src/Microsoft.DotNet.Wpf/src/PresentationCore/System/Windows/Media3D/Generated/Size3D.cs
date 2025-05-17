98b2f98f837936c91e7ad7cac36b9d1f4fdfef10
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:48:27 +0200

    freeze-core (run)
    (cherry picked from commit 698c78a44724d44840dc81ae023ac782cb611519)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Size3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Size3D.cs
@@ -207,7 +207,6 @@ namespace System.Windows.Media.Media3D
         #region Protected Methods
 
 
-
         #endregion ProtectedMethods
 
         //------------------------------------------------------
