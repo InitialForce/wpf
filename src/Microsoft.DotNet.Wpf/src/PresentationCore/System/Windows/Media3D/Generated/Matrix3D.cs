0c411538ee3f164c148ddd411d92e77f48241135
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:09:11 +0200

    write-effective-values (run)
    (cherry picked from commit 7e376e2aaa2210c71ba85fdf9df0bb95cf7b7006)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Matrix3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Matrix3D.cs
@@ -291,7 +291,6 @@ namespace System.Windows.Media.Media3D
 
         #region Internal Properties
 
-
         /// <summary>
         /// Creates a string representation of this object based on the current culture.
         /// </summary>
