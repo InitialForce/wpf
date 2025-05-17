52db68d19242634672f52c7c3d4281ee2f09fff4
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:06:15 +0200

    write-skip-properties (run)
    (cherry picked from commit 26fac7dbf355f8cc4237a104a1771a3d6e5c5877)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Rect3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Rect3D.cs
@@ -209,7 +209,6 @@ namespace System.Windows.Media.Media3D
         #region Public Properties
 
 
-
         #endregion Public Properties
 
         //------------------------------------------------------
