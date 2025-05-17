17d2506b4b81d83f6be21098a8f2d8c3de9aac2f
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 20:50:16 +0200

    create-instance-core newline (mcg edit)
    (cherry picked from commit 3c2055a7cbbe2f2ea8fd5dce34e10a2f12ca74a0)

--- src/Microsoft.DotNet.Wpf/src/WpfGfx/codegen/mcg/generators/ManagedResource.cs
+++ src/Microsoft.DotNet.Wpf/src/WpfGfx/codegen/mcg/generators/ManagedResource.cs
@@ -1477,7 +1477,8 @@ namespace MS.Internal.MilCodeGen.Generators
 
         private string WriteCreateInstanceCore(McgResource resource)
         {
-            if (resource.IsAbstract || resource.IsValueType) return String.Empty;
+            if (resource.IsAbstract || resource.IsValueType)
+				return null;
 
             if (resource.CreateInstanceCoreViaActivator)
             {
