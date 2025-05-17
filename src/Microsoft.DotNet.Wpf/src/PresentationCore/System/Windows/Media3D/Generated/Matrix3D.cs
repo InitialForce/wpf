153796c5f4effa97116b92ef78261d75bf8c3693
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:06:15 +0200

    write-skip-properties (run)
    (cherry picked from commit 26fac7dbf355f8cc4237a104a1771a3d6e5c5877)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Matrix3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Matrix3D.cs
@@ -256,7 +256,6 @@ namespace System.Windows.Media.Media3D
         #region Public Properties
 
 
-
         #endregion Public Properties
 
         //------------------------------------------------------
