0a4fedfa7cabef18c0c7ceacb353177ce2a71044
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 23:59:45 +0200

    to-string-whitespace [run]
    (cherry picked from commit 26ebe5f6ab2e068c64a3c031b29c3b8255b529d2)

--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Media/Generated/Matrix.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Media/Generated/Matrix.cs
@@ -246,7 +246,6 @@ namespace System.Windows.Media
         /// </returns>
         public override string ToString()
         {
-
             // Delegate to the internal method which implements all ToString calls.
             return ConvertToString(null /* format string */, null /* format provider */);
         }
@@ -260,7 +259,6 @@ namespace System.Windows.Media
         /// </returns>
         public string ToString(IFormatProvider provider)
         {
-
             // Delegate to the internal method which implements all ToString calls.
             return ConvertToString(null /* format string */, provider);
         }
@@ -276,7 +274,6 @@ namespace System.Windows.Media
         /// </returns>
         string IFormattable.ToString(string format, IFormatProvider provider)
         {
-
             // Delegate to the internal method which implements all ToString calls.
             return ConvertToString(format, provider);
         }
