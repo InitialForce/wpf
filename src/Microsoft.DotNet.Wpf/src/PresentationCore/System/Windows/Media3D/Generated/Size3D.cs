b8d3324a8d06bf31f8bc278be06cd7d4ba1d9015
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 23:59:45 +0200

    to-string-whitespace [run]
    (cherry picked from commit 26ebe5f6ab2e068c64a3c031b29c3b8255b529d2)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Size3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Size3D.cs
@@ -233,7 +233,6 @@ namespace System.Windows.Media.Media3D
         /// </returns>
         public override string ToString()
         {
-
             // Delegate to the internal method which implements all ToString calls.
             return ConvertToString(null /* format string */, null /* format provider */);
         }
@@ -247,7 +246,6 @@ namespace System.Windows.Media.Media3D
         /// </returns>
         public string ToString(IFormatProvider provider)
         {
-
             // Delegate to the internal method which implements all ToString calls.
             return ConvertToString(null /* format string */, provider);
         }
@@ -263,7 +261,6 @@ namespace System.Windows.Media.Media3D
         /// </returns>
         string IFormattable.ToString(string format, IFormatProvider provider)
         {
-
             // Delegate to the internal method which implements all ToString calls.
             return ConvertToString(format, provider);
         }
