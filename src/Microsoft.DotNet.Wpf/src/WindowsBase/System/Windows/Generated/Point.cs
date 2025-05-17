e85d8f76926aa79ea6555db7b2e798d93e2cdd8b
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:25:17 +0200

    write-register-dps (run)
    (cherry picked from commit 08e38b756c390d954a97f83c4d8cc352479cd379)

--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Generated/Point.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Generated/Point.cs
@@ -308,7 +308,6 @@ namespace System.Windows
         #region Dependency Properties
 
 
-
         #endregion Dependency Properties
 
         //------------------------------------------------------
