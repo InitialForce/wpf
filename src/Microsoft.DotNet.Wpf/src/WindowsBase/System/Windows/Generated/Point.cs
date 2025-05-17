41efdebd4c00c9076a70c67c74b557bf749de489
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:09:11 +0200

    write-effective-values (run)
    (cherry picked from commit 7e376e2aaa2210c71ba85fdf9df0bb95cf7b7006)

--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Generated/Point.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Generated/Point.cs
@@ -232,7 +232,6 @@ namespace System.Windows
 
         #region Internal Properties
 
-
         /// <summary>
         /// Creates a string representation of this object based on the current culture.
         /// </summary>
