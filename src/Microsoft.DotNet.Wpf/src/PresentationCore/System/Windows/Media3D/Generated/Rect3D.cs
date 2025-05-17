257fdf5c1fd297de9e8eab32e312ba5c508ef82a
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:25:17 +0200

    write-register-dps (run)
    (cherry picked from commit 08e38b756c390d954a97f83c4d8cc352479cd379)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Rect3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Rect3D.cs
@@ -329,7 +329,6 @@ namespace System.Windows.Media.Media3D
         #region Dependency Properties
 
 
-
         #endregion Dependency Properties
 
         //------------------------------------------------------
