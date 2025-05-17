5e8adbd4a77180296acdf8bafd35e5a848a8ed5b
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 20:24:49 +0200

    collection fields run
    (cherry picked from commit 00ee749ca080ca7cf02995b3c88f34500160e1eb)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Point4D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Point4D.cs
@@ -385,7 +385,6 @@ namespace System.Windows.Media.Media3D
 
 
 
-
         #endregion Internal Fields
 
 
--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Size3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Size3D.cs
@@ -341,7 +341,6 @@ namespace System.Windows.Media.Media3D
 
 
 
-
         #endregion Internal Fields
 
 
