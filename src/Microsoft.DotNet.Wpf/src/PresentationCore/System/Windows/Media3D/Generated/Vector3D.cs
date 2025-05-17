f0f2e362ab7a19961e25c506e25d9126c492c6bd
h3xds1nz <gamingshard@protonmail.com>
2025-05-17 21:48:27 +0200

    freeze-core (run)
    (cherry picked from commit 698c78a44724d44840dc81ae023ac782cb611519)

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Vector3D.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media3D/Generated/Vector3D.cs
@@ -234,7 +234,6 @@ namespace System.Windows.Media.Media3D
         #region Protected Methods
 
 
-
         #endregion ProtectedMethods
 
         //------------------------------------------------------
