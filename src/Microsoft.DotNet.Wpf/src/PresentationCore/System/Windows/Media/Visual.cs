20417c32530d52a1de445224882a8abdcce85ce5
Claude (Initial Force WPF Bot) <wpf-bot@initialforce.com>
2026-05-06 00:54:57 +0200

    Revert "wpf-ar(iter=007): retry InternalTransformToAncestor identity short-circuit"
    This reverts commit d148eb7eb503260e25096f0145bab09c8847703d.

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Visual.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Visual.cs
@@ -4435,10 +4435,6 @@ namespace System.Windows.Media
 
             if (isSimple)
             {
-                if (simpleTransform.IsIdentity)
-                {
-                    return Transform.Identity;
-                }
                 MatrixTransform matrixTransform = new MatrixTransform(simpleTransform);
                 matrixTransform.Freeze();
                 return matrixTransform;
