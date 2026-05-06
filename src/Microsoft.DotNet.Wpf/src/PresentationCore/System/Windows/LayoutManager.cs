816cb8aafa8d84268721923b77f6bb7127f3759d
wpf-ar-eval <eval@autoresearch.local>
2026-05-06 09:47:13 +0200

    Revert "wpf-ar(iter=017): hoist parent.TreeLevel out of RemoveOrphans inner loop"
    This reverts commit 49aaa74bde876a07472b1d58988d6e5695f67e76.

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/LayoutManager.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/LayoutManager.cs
@@ -936,15 +936,14 @@ namespace System.Windows
 
             internal void RemoveOrphans(UIElement parent)
             {
-                ulong parentTreeLevel = parent.TreeLevel;
-                ulong childTreeLevel = parentTreeLevel + 1;
                 Request r = _head;
                 while(r != null)
                 {
                     UIElement child = r.Target;
                     Request next = r.Next;
+                    ulong parentTreeLevel = parent.TreeLevel;
 
-                    if(   (child.TreeLevel == childTreeLevel)
+                    if(   (child.TreeLevel == parentTreeLevel + 1)
                        && (child.GetUIParentWithinLayoutIsland() == parent))
                     {
                         _removeRequest(getRequest(child));
